ddc0a73027f220139f62a6c4eedfd4aaa9376cd6
Will Thomas <wlthomas001@gmail.com>
2018-05-28 19:02:20 -0600

    finished solution for quiz game gophercise

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type Question struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+func main() {
+	questions := readFileAndParse("problems.csv")
+	reader := bufio.NewReader(os.Stdin)
+	answersCorrect := 0
+	numOfQuestions := 0
+
+	for _, v := range questions {
+		numOfQuestions = numOfQuestions + 1
+		var answer string
+		fmt.Printf("What is: %s?    ", v.Question)
+		answer, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("There was an error, reading your answer")
+		}
+		sanitizedAnswer := strings.TrimSpace(answer)
+		if strings.Compare(v.Answer, sanitizedAnswer) == 0 {
+			fmt.Println("You are correct.")
+			answersCorrect = answersCorrect + 1
+		} else {
+			fmt.Println("Incorrect.")
+		}
+	}
+
+	fmt.Printf("Quiz finished, you got %v/%v right. \n", answersCorrect, numOfQuestions)
+
+}
+
+func readFileAndParse(fileName string) []Question {
+	csvFile, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	var questions []Question
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+		q := strings.Join(line[:len(line)-1], ",")
+		a := line[len(line)-1]
+
+		questions = append(questions, Question{q, a})
+	}
+	return questions
+}
